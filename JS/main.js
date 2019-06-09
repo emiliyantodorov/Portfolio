07c1527e9ff7d9480c7efc64ee35316559ec46df
Emiliyan Todorov <emiliyantodorov@abv.bg>
2019-06-10 00:35:45 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    start();
+});
+
+function start() {
+    let mobileMenuButton = $(".mobile-menu-button");
+    let navBarEl = $(".nav-bar");
+    let topLine = $(".line--top");
+    let centerLineEl = $(".line--center");
+    let bottomLine = $(".line--bottom");
+
+
+    mobileMenuButton.on("click", function (evt) {
+        evt.preventDefault();
+        // evt.stopPropagation();
+        //Showing the menu
+        navBarEl.slideToggle();
+
+        //Animation
+        if (centerLineEl.hasClass("visible")) {
+
+            navBarEl.css("display", "block");
+            centerLineEl
+                .removeClass("visible")
+                .css("display", "none");
+
+            topLine
+                .css("transform", "rotate(45deg)")
+                .css("margin-bottom", "0");
+            bottomLine
+                .css("bottom", ".5rem")
+                .css("transform", "rotate(-45deg)")
+                .css("position", "relative")
+                .css("bottom", ".5rem")
+        } else {
+
+            navBarEl.css("display", "block");
+
+            topLine
+                .css("transform", "rotate(0)")
+                .css("margin-bottom", "1.4rem");
+
+            bottomLine
+                .css("transform", "rotate(0)")
+                .css("bottom", "0");
+            setTimeout(function () {
+                centerLineEl
+                    .addClass("visible")
+                    .css("display", "block");
+            }, 300);
+        }
+    });
+
+    //Sliding to section on link click
+    let allLiks = $(".nav-bar__item--link");
+
+    allLiks.on("click", function (evt) {
+        let targetElement = $(evt.target).attr("href");
+        let targetPosition = $(targetElement).offset().top;
+
+        $("html, body").animate({scrollTop: targetPosition}, "slow");
+    })
+}
